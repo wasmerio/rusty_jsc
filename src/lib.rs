c4c242ae211a7c508055e9d7e783d75071b9f3e0
Pekka Enberg <penberg@iki.fi>
2022-08-27 08:24:52 +0300

    Improve documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,30 @@
+//! This library provides a Rust API for the <a href="https://developer.apple.com/documentation/javascriptcore">JavaScriptCore</a> engine.
+//!
+//! # Example
+//!
+//! The JavaScriptCore engine lets you evaluate JavaScript scripts from your
+//! own application. You first need to create a `JSContext` object and then
+//! call its `evaluate_script` method.
+//!
+//! ```rust
+//! let mut context = JSContext::default();
+//! match context.evaluate_script("'hello, world'", 1) {
+//!     Some(value) => {
+//!         println!("{}", value.to_string(&context));
+//!     }
+//!     None => {
+//!         println!(
+//!             "Uncaught: {}",
+//!             context.get_exception().unwrap().to_string(&context)
+//!         )
+//!     }
+//! }
+//! ```
+
 use rusty_jsc_sys::*;
 use std::ffi::CString;
 
+/// A JavaScript value.
 #[derive(Debug)]
 pub struct JSValue {
     inner: JSValueRef,
@@ -13,14 +37,17 @@ impl Drop for JSValue {
 }
 
 impl JSValue {
+    /// Wraps a `JSValue` from a `JSValueRef`.
     fn from(inner: JSValueRef) -> Self {
         Self { inner }
     }
 
+    /// Checks if this value is `null`.
     fn is_null(&self, context: &JSContext) -> bool {
         unsafe { JSValueIsNull(context.global_context, self.inner) }
     }
 
+    /// Formats this value as a `String`.
     pub fn to_string(&self, context: &JSContext) -> String {
         let mut exception: JSValueRef = std::ptr::null_mut();
         let s = unsafe { JSValueToStringCopy(context.global_context, self.inner, &mut exception) };
@@ -29,6 +56,7 @@ impl JSValue {
     }
 }
 
+/// A JavaScript string.
 pub struct JSString {
     inner: JSStringRef,
 }
@@ -81,6 +109,7 @@ impl Drop for JSContext {
 }
 
 impl JSContext {
+    /// Create a new `JSContext` object.
     pub fn new() -> Self {
         let context_group = unsafe { JSContextGroupCreate() };
         let global_context =
@@ -92,10 +121,16 @@ impl JSContext {
         }
     }
 
+    /// Return the exception thrown while evaluating a script.
     pub fn get_exception(&self) -> Option<&JSValue> {
         self.exception.as_ref()
     }
 
+    /// Evaluate the script.
+    ///
+    /// Returns the value the script evaluates to. If the script throws an
+    /// exception, this function returns `None`. You can query the thrown
+    /// exception with the `get_exception` method.
     pub fn evaluate_script(&mut self, script: &str, starting_line_number: i32) -> Option<JSValue> {
         self.exception = None;
         let script = CString::new(script.as_bytes()).unwrap();
