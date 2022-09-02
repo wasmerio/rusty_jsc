4db300e3eb4ae609f04e3e2f954db36917129db9
Pekka Enberg <penberg@iki.fi>
2022-09-02 16:13:32 +0300

    Add "callback" macro

--- examples/callback.rs
+++ examples/callback.rs
@@ -1,27 +1,15 @@
 use rusty_jsc::{JSContext, JSValue};
-use rusty_jsc_sys::*;
+use rusty_jsc_macros::callback;
 
 // The JavaScript code calls this Rust function.
-fn foo() {
+#[callback]
+fn foo(_context: JSContext) {
     println!("hello from Rust land!");
-}
-
-// TODO: hide this in a macro magic.
-unsafe extern "C" fn foo_trampoline(
-    ctx: JSContextRef,
-    _function: JSObjectRef,
-    _this_object: JSObjectRef,
-    _argument_count: size_t,
-    _arguments: *const JSValueRef,
-    _exception: *mut JSValueRef,
-) -> JSValueRef {
-    foo();
-    JSValueMakeUndefined(ctx)
 }
 
 fn main() {
     let mut context = JSContext::default();
-    let callback = JSValue::callback(&context, Some(foo_trampoline));
+    let callback = JSValue::callback(&context, Some(foo));
     let mut global = context.get_global_object();
     global.set_property(&context, "foo".to_string(), callback);
     match context.evaluate_script("foo()", 1) {
---
+++ macros/src/lib.rs
@@ -0,0 +1,33 @@
+use proc_macro::TokenStream;
+use quote::quote;
+
+#[proc_macro_attribute]
+pub fn callback(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let func = syn::parse::<syn::ItemFn>(item).expect("expected a function");
+    let name = &func.sig.ident;
+    let target_func = {
+        let mut func = func.clone();
+        func.sig.ident = quote::format_ident!("call");
+        func
+    };
+    let target_name = &target_func.sig.ident;
+    let result = quote! {
+        use rusty_jsc_sys::*;
+        
+        #target_func
+
+        unsafe extern "C" fn #name(
+            ctx: JSContextRef,
+            _function: JSObjectRef,
+            _this_object: JSObjectRef,
+            _argument_count: size_t,
+            _arguments: *const JSValueRef,
+            _exception: *mut JSValueRef,
+        ) -> JSValueRef {
+            let context = JSContext::from(ctx);
+            #target_name(context);
+            JSValueMakeUndefined(ctx)
+        }
+    };
+    result.into()
+}
